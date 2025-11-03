6eab2a28e091d883694283d65fd32f4de627df11
David Lindblom <david@Davids-MacBook-Pro.local>
2025-11-03 09:25:00 +0100

    added data in seperate file and made queryselector variables

---
+++ src/data.js
@@ -0,0 +1,13 @@
+// data.js
+let MOCK_WEATHER = {
+  "Stockholm": { tempC: 7, description: "Mulet", icon: "☁️", updated: "09:00" },
+  "Göteborg": { tempC: 8, description: "Lätt regn", icon: "🌧️", updated: "09:00" },
+  "Malmö": { tempC: 10, description: "Klart", icon: "☀️", updated: "09:00" },
+  "Uppsala": { tempC: 6, description: "Disigt", icon: "🌫️", updated: "09:00" },
+  "Lund": { tempC: 9, description: "Halvklart", icon: "⛅", updated: "09:00" },
+  "Örebro": { tempC: 5, description: "Regn", icon: "🌧️", updated: "09:00" },
+  "Västerås": { tempC: 4, description: "Dimma", icon: "🌁", updated: "09:00" },
+  "Linköping": { tempC: 8, description: "Soligt", icon: "☀️", updated: "09:00" },
+  "Helsingborg": { tempC: 9, description: "Halvklart", icon: "⛅", updated: "09:00" },
+  "Kiruna": { tempC: -3, description: "Snö", icon: "❄️", updated: "09:00" }
+};
