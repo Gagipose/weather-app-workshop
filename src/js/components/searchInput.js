9b5574b41aaaec2ee1b6aba54074616c63c33b74
ahmringholm <ringholmahm4@gmail.com>
2025-11-24 11:14:45 +0100

    added searchInput formatting function

---
+++ src/js/components/searchInput.js
@@ -0,0 +1,23 @@
+export function searchCity() {
+    const input = document.getElementById("searchInput");
+    const button = document.getElementById("searchBtn");
+
+    function formatSearchCity(name) {
+        if (!name) return "";
+        
+        const trimmed = name.trim();
+
+        return (
+            trimmed.charAt(0).toUpperCase() + trimmed.substring(1).toLowerCase()
+        );
+    }
+
+    function runSearchCity() {
+        const city = formatSearchCity(input.value);
+
+        if (!CITIES[city]) {
+            document.getElementById("container").textContent = `Staden finns inte...`;
+            return;
+        }
+    }
+}
